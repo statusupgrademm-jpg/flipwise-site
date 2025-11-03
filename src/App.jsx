eed77de57ce4e3d66e9174fe93dfa0185ba7a0e7
statusupgrademm-jpg <statusupgrademm@gmail.com>
2025-11-03 00:06:50 -0800

    change-bg-color-of-post-card

--- src/App.jsx
+++ src/App.jsx
@@ -195,7 +195,7 @@ function BlogCard({ post, onOpen }) {
           className="w-full h-full object-cover"
           decoding="async"
         />
-        <div className="absolute inset-0 bg-black/50" />
+        <div className="absolute inset-0 bg-black/70" />
       </div>
 
       {/* Foreground content */}
